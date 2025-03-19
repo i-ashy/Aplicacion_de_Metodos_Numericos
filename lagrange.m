fe29e815af98606f0478f9f62c32f6d39ee85250
Ashy <101229247+i-ashy@users.noreply.github.com>
2025-03-19 00:27:01 -0600

    Update lagrange.m

--- lagrange.m
+++ lagrange.m
@@ -3,7 +3,7 @@ disp('Metodo de Interpolacion de Lagrange')
 X=input('Ingrese los valores de x: ');
 Y=input('Ingrese los valores de y: ');
 aprox=input('Ingrese el valor a aproximar: ');
-n=length(X); %Obteniendo el size del vector
+n=length(X); 
 for i=1:n
     numerador=1;
     denominador=1;
@@ -17,7 +17,6 @@ for i=1:n
     fprintf('L%d(x)=\n', i-1);
     pretty(L(i));
 end
-%construyendo el polinomio
 pol=0;
 for i=1:n
     pol=pol+double(Y(i))*L(i);
